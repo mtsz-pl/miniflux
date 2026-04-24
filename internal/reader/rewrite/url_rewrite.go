af54faed106d8f4842ca9059731607831469297d
Mateusz Jabłoński <github.com@mtsz.pl>
2026-04-24 09:28:29 +0200

    feat(ui): replace rule inputs with textareas for multiline support
    It is valid to use multiple rules for feed processing, and textarea  with multiline support is more suitable for display/editing, than single-lined input. All rules properly handle whitespaces and newlines, so there is no regression.

--- internal/reader/rewrite/url_rewrite.go
+++ internal/reader/rewrite/url_rewrite.go
@@ -6,6 +6,7 @@ package rewrite // import "miniflux.app/v2/internal/reader/rewrite"
 import (
 	"log/slog"
 	"regexp"
+	"strings"
 
 	"miniflux.app/v2/internal/model"
 )
@@ -17,17 +18,39 @@ func RewriteEntryURL(feed *model.Feed, entry *model.Entry) string {
 		return entry.URL
 	}
 
+	rewrittenURL := entry.URL
+
+	for line := range strings.SplitSeq(strings.TrimSpace(feed.UrlRewriteRules), "\n") {
+		rule := strings.TrimSpace(line)
+		if rule == "" {
+			continue
+		}
+
+		newURL, valid := rewriteEntryURLRule(rule, feed, entry)
+		if !valid {
+			return entry.URL
+		}
+
+		rewrittenURL = newURL
+	}
+
+	return rewrittenURL
+}
+
+// rewriteEntryURLRule applies a single rewrite rule to the given URL.
+// It returns the rewritten URL, and a boolean indicating if the rule is valid.
+func rewriteEntryURLRule(rule string, feed *model.Feed, entry *model.Entry) (string, bool) {
 	var rewrittenURL = entry.URL
-	parts := customReplaceRuleRegex.FindStringSubmatch(feed.UrlRewriteRules)
+	parts := customReplaceRuleRegex.FindStringSubmatch(rule)
 
 	if len(parts) == 3 {
 		re, err := regexp.Compile(parts[1])
 		if err != nil {
 			slog.Error("Failed on regexp compilation",
-				slog.String("url_rewrite_rules", feed.UrlRewriteRules),
+				slog.String("url_rewrite_rules", rule),
 				slog.Any("error", err),
 			)
-			return rewrittenURL
+			return rewrittenURL, false
 		}
 		rewrittenURL = re.ReplaceAllString(entry.URL, parts[2])
 		slog.Debug("Rewriting entry URL",
@@ -42,9 +65,10 @@ func RewriteEntryURL(feed *model.Feed, entry *model.Entry) string {
 			slog.String("rewritten_entry_url", rewrittenURL),
 			slog.Int64("feed_id", feed.ID),
 			slog.String("feed_url", feed.FeedURL),
-			slog.String("url_rewrite_rules", feed.UrlRewriteRules),
+			slog.String("url_rewrite_rules", rule),
 		)
+		return rewrittenURL, false
 	}
 
-	return rewrittenURL
+	return rewrittenURL, true
 }
